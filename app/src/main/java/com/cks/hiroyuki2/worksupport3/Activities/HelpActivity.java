e65b357b9c83c939a5cf26e623c2be33c3151e2d
hiroyuki <hiroyukitamura@gmail.com>
2017-11-05 21:42:28 +0900

    ヘルプ画面作成に着手するもよう

---
+++ app/src/main/java/com/cks/hiroyuki2/worksupport3/Activities/HelpActivity.java
@@ -0,0 +1,49 @@
+package com.cks.hiroyuki2.worksupport3.Activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.cks.hiroyuki2.worksupport3.Fragments.GroupSettingFragment;
+import com.cks.hiroyuki2.worksupport3.Fragments.HelpFragment;
+import com.cks.hiroyuki2.worksupport3.R;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+import static com.cks.hiroyuki2.worksupport3.Util.initAdMob;
+
+@EActivity(R.layout.activity_help)
+public class HelpActivity extends AppCompatActivity {
+
+    @ViewById(R.id.toolbar) Toolbar toolbar;
+
+    @AfterViews
+    void afterViews(){
+        if(getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setSupportActionBar(toolbar);
+        initAdMob(this);
+        setFragment();
+    }
+
+    private void setFragment(){
+        FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+        HelpFragment fragment = HelpFragment_.builder().build();
+        t.add(R.id.fragment_container, fragment).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            super.onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/cks/hiroyuki2/worksupport3/Fragments/HelpFragment.java
@@ -0,0 +1,22 @@
+package com.cks.hiroyuki2.worksupport3.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cks.hiroyuki2.worksupport3.R;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EFragment;
+
+@EFragment(R.layout.fragment_help)
+public class HelpFragment extends Fragment {
+
+    @AfterViews
+    void afterViews(){
+        
+    }
+}
