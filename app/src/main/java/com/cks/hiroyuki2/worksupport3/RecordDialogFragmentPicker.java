d64098eee195ba5643804a57e5fb115e817b1d45
hiroyuki <hiroyukitamura@gmail.com>
2017-11-12 18:09:41 +0900

    rangeで前後関係がおかしくならないように変更

--- app/src/main/java/com/cks/hiroyuki2/worksupport3/RecordDialogFragmentPicker.java
+++ app/src/main/java/com/cks/hiroyuki2/worksupport3/RecordDialogFragmentPicker.java
@@ -16,34 +16,47 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 import com.cks.hiroyuki2.worksupprotlib.Entity.TimeEvent;
+import com.cks.hiroyuki2.worksupprotlib.Entity.TimeEventRange;
 import com.cks.hiroyuki2.worksupprotlib.UtilDialog;
 
+import java.sql.Time;
+
+import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 import static android.app.Activity.RESULT_OK;
+import static com.cks.hiroyuki2.worksupprotlib.Util.TIME_EVENT_RANGE;
+import static com.cks.hiroyuki2.worksupprotlib.Util.getMimeType;
 import static com.cks.hiroyuki2.worksupprotlib.Util.onError;
 import static com.example.hiroyuki3.worksupportlibw.Adapters.TimeEventRVAdapter.TIME_EVENT;
 import static com.example.hiroyuki3.worksupportlibw.Adapters.TimeEventRangeRVAdapter.CALLBACK_RANGE_CLICK_TIME;
+import static com.example.hiroyuki3.worksupportlibw.Adapters.TimeEventRangeRVAdapter.POSITION;
 
 /**
  * ダイアログ作成おじさん！
  */
-public class RecordDialogFragmentPicker extends DialogFragment implements DialogInterface.OnClickListener, TimePickerDialog.OnTimeSetListener{
+public class RecordDialogFragmentPicker extends DialogFragment implements DialogInterface.OnClickListener, TimePickerDialog.OnTimeSetListener, TimePicker.OnTimeChangedListener, RadioGroup.OnCheckedChangeListener{
 
     private static final String TAG = "MANUAL_TAG: " + RecordDialogFragmentPicker.class.getSimpleName();
     static final String DIALOG_TIME_TIME = "DIALOG_TIME";
     private TimeEvent timeEvent;
+    private TimeEventRange range;
+    private int pos;
     @BindView(R.id.time_picker) TimePicker timePicker;
     @BindView(R.id.radio_group) RadioGroup radioGroup;
     @BindView(R.id.today) RadioButton radioToday;
     @BindView(R.id.yesterday) RadioButton radioYesterday;
     @BindView(R.id.tomorrow) RadioButton radioTommorow;
+    @BindView(R.id.tv) TextView tv;
+    @BindString(R.string.tp_range_msg) String msg;
 
     public static RecordDialogFragmentPicker newInstance(Bundle bundle){
         Log.d(TAG, "newInstance: fire");
@@ -55,9 +68,11 @@ public class RecordDialogFragmentPicker extends DialogFragment implements Dialog
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        timeEvent = (TimeEvent) getArguments().getSerializable(TIME_EVENT);
-
         if (getTargetRequestCode() == CALLBACK_RANGE_CLICK_TIME){
+
+            range = (TimeEventRange) getArguments().getSerializable(TIME_EVENT_RANGE);
+            pos = (int) getArguments().getInt(POSITION);
+            timeEvent = range.getTimeEve(pos);
 
             AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
             View view = getActivity().getLayoutInflater().inflate(R.layout.timepicker_custom, null);
@@ -68,9 +83,13 @@ public class RecordDialogFragmentPicker extends DialogFragment implements Dialog
             } else if (timeEvent.getOffset() <0){
                 radioYesterday.toggle();
             }
-            return UtilDialog.editBuilder(builder, null, R.string.ok, R.string.cancel, view, this, null).create();
+            AlertDialog dialog =  UtilDialog.editBuilder(builder, null, R.string.ok, R.string.cancel, view, this, null).create();
+            timePicker.setOnTimeChangedListener(this);
+            radioGroup.setOnCheckedChangeListener(this);
+            return dialog;
 
         } else {
+            timeEvent = (TimeEvent) getArguments().getSerializable(TIME_EVENT);
             TimePickerDialog dialog =  new TimePickerDialog(getActivity(), this, timeEvent.getHour(), timeEvent.getMin(), true);
             dialog.setButton(DialogInterface.BUTTON_NEGATIVE, getContext().getString(R.string.cancel), this);
             return dialog;
@@ -97,18 +116,7 @@ public class RecordDialogFragmentPicker extends DialogFragment implements Dialog
         }
 
         if (getTargetRequestCode() == CALLBACK_RANGE_CLICK_TIME){
-            int offset = 0;
-            switch (radioGroup.getCheckedRadioButtonId()){
-                case R.id.yesterday:
-                    offset = -1;
-                    break;
-                case R.id.today:
-                    break;
-                case R.id.tomorrow:
-                    offset = 1;
-                    break;
-            }
-            timeEvent.setOffset(offset);
+            timeEvent.setOffset(getOffsetFromDialog());
         }
 
         timeEvent.setHour(timePicker.getHour());
@@ -116,5 +124,62 @@ public class RecordDialogFragmentPicker extends DialogFragment implements Dialog
         Intent intent = new Intent();
         intent.putExtras(getArguments());
         target.onActivityResult(getTargetRequestCode(), RESULT_OK, intent);//targetはRecordFragment or EditTemplateFragmentのどちらかです
+    }
+
+    @Override
+    public void onTimeChanged(TimePicker timePicker, int hourOfDay, int min) {
+        onChangeCallback(hourOfDay, min);
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, int i) {
+        onChangeCallback(timePicker.getHour(), timePicker.getMinute());
+    }
+
+    private void onChangeCallback(int hourOfDay, int min){
+        Button okBtn = ((AlertDialog) getDialog()).getButton(DialogInterface.BUTTON_POSITIVE);
+        int pairedPos = pos == 0 ? 1 : 0;
+        TimeEvent paired = range.getTimeEve(pairedPos);
+        TimeEvent eve = new TimeEvent("", 0, hourOfDay, min, getOffsetFromDialog());
+        if (pairedPos == 0 && compare(paired, eve) > 0){
+            tv.setText(msg);
+            okBtn.setEnabled(false);
+        } else if (pairedPos == 1 && compare(eve, paired) > 0){
+            tv.setText(msg);
+            okBtn.setEnabled(false);
+        } else {
+            tv.setText("");
+            okBtn.setEnabled(true);
+        }
+    }
+
+    private int getOffsetFromDialog(){
+        int offset = 0;
+        switch (radioGroup.getCheckedRadioButtonId()){
+            case R.id.yesterday:
+                offset = -1;
+                break;
+            case R.id.today:
+                break;
+            case R.id.tomorrow:
+                offset = 1;
+                break;
+        }
+        return offset;
+    }
+
+    /**
+     * @param eve1 よりも @param eve2 が進んでいた場合、
+     * @return 負の数を返す。
+     */
+    private int compare(TimeEvent eve1, TimeEvent eve2){
+
+        if (eve1.getOffset() != eve2.getOffset())
+            return eve1.getOffset() - eve2.getOffset();
+
+        if (eve1.getHour() != eve2.getHour())
+            return eve1.getHour() - eve2.getHour();
+
+        return eve1.getMin() - eve2.getMin();
     }
 }
